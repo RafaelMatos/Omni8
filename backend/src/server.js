2fe636b6a46cd95ab227c9e3b1e2bf34b524f41b
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2020-11-19 13:03:05 -0300

    Socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,28 @@ const mongoose = require('mongoose') //Importante ser antes das rotas,pq?
 const routes = require('./routes')
 const cors = require('cors')
 
-mongoose.connect('mongodb+srv://teste:teste@cluster0.mp4en.mongodb.net/week8?retryWrites=true&w=majority',{
-    useNewUrlParser : true,
-     useUnifiedTopology: true ,
+
+const app = express()
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) =>{
+    console.log('Nova conexão', socket.id)
+    socket.on('hello', message => {
+        console.log(message)
+    })
 })
 
-const server = express()
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+mongoose.connect('mongodb+srv://teste:teste@cluster0.mp4en.mongodb.net/week8?retryWrites=true&w=majority',{
+    useNewUrlParser : true,
+    
+})
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 server.listen(3333)
